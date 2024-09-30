989e59543c3da94ad19e6495debd14fe6ec1982b
xthebasicx <thebasic@hotmail.com>
2024-09-30 14:53:48 +0700

    create project

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CoreModule } from './core/core.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const user = configService.get<string>('DB_USER');
+        const pass = configService.get<string>('DB_PASS');
+        const dbName = configService.get<string>('DB_NAME');
+
+        return {
+          uri: `mongodb://${user}:${pass}@localhost:27017/${dbName}?authSource=admin`,
+        };
+      },
+    }),
+    CoreModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('IInterested API')
+    .setDescription('note everything you are interested in')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  const app_port = 3000;
+  await app.listen(app_port);
+
+  Logger.log(`Server Start on http://localhost:${app_port}`);
+  Logger.log(`Swagger Start on http://localhost:${app_port}/swagger`);
+}
+bootstrap();
